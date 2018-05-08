656875b5b12354d718f40b5754fa73147d5090f1
Michael Mao <myxmichael95@gmail.com>
2018-05-08 17:48:49 +0800

    1. update gradle to support annotations 2. update LocationServiceFragement to fetch the GPS data per 5 minutes TODO : store the data and calculate whether some task has been accomplished

--- app/src/main/java/hk/com/csci4140/culife/fragment/LocationServiceFragment.java
+++ app/src/main/java/hk/com/csci4140/culife/fragment/LocationServiceFragment.java
@@ -7,6 +7,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -24,15 +25,23 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.cazaea.sweetalert.SweetAlertDialog;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.ActivityTransitionRequest;
 import com.google.android.gms.location.DetectedActivity;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -41,6 +50,8 @@ import hk.com.csci4140.culife.Constant;
 import hk.com.csci4140.culife.R;
 import hk.com.csci4140.culife.activity.LoginActivity;
 import hk.com.csci4140.culife.activity.MainActivity;
+import io.reactivex.annotations.NonNull;
+
 import com.google.android.gms.location.ActivityRecognition;
 import com.google.android.gms.location.ActivityTransition;
 import com.google.android.gms.tasks.Task;
@@ -53,6 +64,11 @@ public class LocationServiceFragment extends BaseFragment {
     private String mPrevTitle;
 
     private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
+    private Location mLocation;
+
+
 
 
     //Initial Setting of every fragment
@@ -104,21 +120,6 @@ public class LocationServiceFragment extends BaseFragment {
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
     }
 
-//    @Override
-//    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-//        super.onCreateOptionsMenu(menu, inflater);
-//        //In case the duplicate menu item
-//        try {
-//            for(int i = 0; i < menu.size(); i ++){
-//                menu.getItem(i).setVisible(false);
-//            }
-//        }catch (Exception e){
-//            Log.e(TAG, "onCreateOptionsMenu: " + e.toString());
-//        }
-//
-//        inflater.inflate(R.menu.user_profile_menu, menu);
-//    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View mView = inflater.inflate(R.layout.fragment_habit_detail, container, false);
@@ -145,9 +146,37 @@ public class LocationServiceFragment extends BaseFragment {
 
     @OnClick(R.id.confirm_complete)
     void onClickConfirmComplete(){
-        checkLastLocation();
+//        checkLastLocation();
 //        checkUserActivity();
+        createLocationRequest();
+        try {
+            checkLocationSetting();
+        }catch (Exception e){
+            Log.d("Location", "error: "+e);
+        }
+        startLocationUpdates();
     }
+
+
+
+
+    // the page is dismissed
+    @Override
+    public void onDestroy(){
+        stopLocationUpdates();
+        super.onDestroy();
+        //Set the title of previous fragment
+        setToolbarTitle(mPrevTitle);
+    }
+
+
+
+
+
+
+
+
+    // additional functions
 
     public void checkLastLocation(){
         if (ContextCompat.checkSelfPermission(getActivity(),
@@ -168,6 +197,7 @@ public class LocationServiceFragment extends BaseFragment {
                         public void onSuccess(Location location) {
                             if (location != null) {
                                 Toast.makeText(getContext(), "get the location back", Toast.LENGTH_SHORT).show();
+                                mLocation = location;
                             }else{
                                 Toast.makeText(getContext(), "no location back", Toast.LENGTH_SHORT).show();
                             }
@@ -179,16 +209,6 @@ public class LocationServiceFragment extends BaseFragment {
     public void checkUserActivity(){
 
         List<ActivityTransition> transitions = new ArrayList<>();
-//        transitions.add(
-//                new ActivityTransition.Builder()
-//                        .setActivityType(DetectedActivity.IN_VEHICLE)
-//                        .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_ENTER)
-//                        .build());
-//        transitions.add(
-//                new ActivityTransition.Builder()
-//                        .setActivityType(DetectedActivity.IN_VEHICLE)
-//                        .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
-//                        .build());
 
         transitions.add(
                 new ActivityTransition.Builder()
@@ -230,7 +250,7 @@ public class LocationServiceFragment extends BaseFragment {
 
 
 
-//        Toast.makeText(getContext(), "Into check user activity", Toast.LENGTH_SHORT).show();
+
 
         Intent intent = new Intent();
         intent.setAction("myaction");
@@ -275,8 +295,6 @@ public class LocationServiceFragment extends BaseFragment {
 
 
 
-//        Toast.makeText(getContext(), "end check user activity", Toast.LENGTH_SHORT).show();
-
 
 //        // myPendingIntent is the instance of PendingIntent where the app receives callbacks.
 //        Task<Void> deRegisterTask =
@@ -300,13 +318,71 @@ public class LocationServiceFragment extends BaseFragment {
 
     }
 
-    // the page is dismissed
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+//        mLocationRequest.setInterval(1000*60*10);
+//        mLocationRequest.setFastestInterval(50000);
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(100);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
 
-        //Set the title of previous fragment
-        setToolbarTitle(mPrevTitle);
+    public void checkLocationSetting(){
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+        SettingsClient client = LocationServices.getSettingsClient(getContext());
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+        task.addOnSuccessListener(getActivity(), new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                Log.d("Location Info", "LocationSettingsResponse is OK");
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                // ...
+            }
+        });
+        task.addOnFailureListener(getActivity(), new OnFailureListener() {
+            protected static final int REQUEST_CHECK_SETTINGS = 0x1;
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("Location Info", "LocationSettingsResponse is Failure");
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(), and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(getActivity(),
+                                REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                    }
+                }
+            }
+        });
+    }
+
+    private void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+        }else{
+            mLocationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    if (locationResult == null) {
+                        return;
+                    }
+                    for (Location location : locationResult.getLocations()) {
+                        Log.d("Location Info", "onLocationResult: "+location);
+                    }
+                }
+            };
+
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                    mLocationCallback,
+                    null /* Looper */);
+        }
+    }
+
+    private void stopLocationUpdates() {
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
     }
 
 }
